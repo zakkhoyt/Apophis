9967f9cd816031b86a0f89827272dd2653516770
Zakk Hoyt <vaporwarewolf@gmail.com>
2014-03-13 20:35:21 -0700

    Initial Commit

---
+++ Apophis/VWWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWWAppDelegate.h
+//  Apophis
+//
+//  Created by Zakk Hoyt on 3/13/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Apophis/VWWViewController.h
@@ -0,0 +1,13 @@
+//
+//  VWWViewController.h
+//  Apophis
+//
+//  Created by Zakk Hoyt on 3/13/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWWViewController : UIViewController
+
+@end
